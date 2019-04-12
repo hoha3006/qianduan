2c54ccc452e9cd1f8baf8623d939451e5d438f02
DavidXu2017 <davidxu198556@gmail.com>
2019-04-11 23:01:40 -0700

    add temp ui: search bar, swiper, main button and tool bar

--- servicing/dashboard/src/pages/index/index.js
+++ servicing/dashboard/src/pages/index/index.js
@@ -1,11 +1,22 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-
+import { AtButton } from 'taro-ui'
 import { add, minus, asyncAdd } from '../../actions/counter'
-
+import { AtIcon } from 'taro-ui'
+import { AtTabBar } from 'taro-ui'
+import { AtGrid } from "taro-ui"
+import { AtSearchBar } from 'taro-ui'
+import { Swiper, SwiperItem } from '@tarojs/components'
 import './index.css'
 
+import "taro-ui/dist/style/components/button.scss";
+import "taro-ui/dist/style/components/tab-bar.scss";
+import "taro-ui/dist/style/components/badge.scss";
+import "taro-ui/dist/style/components/grid.scss";
+
+import "taro-ui/dist/style/components/search-bar.scss";
+import "taro-ui/dist/style/components/icon.scss";
 
 @connect(({ counter }) => ({
   counter
@@ -21,6 +32,27 @@ import './index.css'
   }
 }))
 class Index extends Component {
+
+  constructor () {
+    super(...arguments)
+    this.state = {
+      current: 0,
+      value: ''
+    }
+  }
+  handleClick (value) {
+    this.setState({
+      current: value
+    })
+  }
+  onChange (value) {
+    this.setState({
+      value: value
+    })
+  }
+  onActionClick () {
+    console.log('开始搜索')
+  }
 
     config = {
     navigationBarTitleText: '首页'
@@ -40,6 +72,74 @@ class Index extends Component {
     return (
       <View className='index'>
         <View style={{textAlign: "center", marginTop: "20px"}}><Text>吼哈科技</Text></View>
+        <AtSearchBar
+          showActionButton
+          value={this.state.value}
+          onChange={this.onChange.bind(this)}
+          onActionClick={this.onActionClick.bind(this)}
+        />
+        <Swiper
+          className='test-h'
+          indicatorColor='#999'
+          indicatorActiveColor='#333'
+          vertical
+          circular
+          indicatorDots
+          autoplay>
+          <SwiperItem>
+            <View className='demo-text-1'>
+              <img className="taro-img__mode-scaletofill" src="https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180" />
+            </View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='demo-text-2'>
+              <img class="taro-img__mode-scaletofill" src="https://img14.360buyimg.com/babel/s700x360_jfs/t1/4099/12/2578/101668/5b971b4bE65ae279d/89dd1764797acfd9.jpg!q90!cc_350x180"/>
+            </View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='demo-text-3'>
+              <img class="taro-img__mode-scaletofill" src="https://img10.360buyimg.com/babel/s700x360_jfs/t25855/203/725883724/96703/5a598a0f/5b7a22e1Nfd6ba344.jpg!q90!cc_350x180"/>
+            </View>
+          </SwiperItem>
+        </Swiper>
+        <AtGrid columnNum={3} data={
+          [
+            {
+              image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
+              value: '领取中心'
+            },
+            {
+              image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
+              value: '找折扣'
+            },
+            {
+              image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
+              value: '领会员'
+            },
+            {
+              image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
+              value: '新品首发'
+            },
+            {
+              image: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
+              value: '领京豆'
+            },
+            {
+              image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+              value: '手机馆'
+            }
+          ]
+        } />
+        <AtTabBar
+          fixed
+          tabList={[
+            { title: '搜索', image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png', selectedImage: 'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png', text: 'new' },
+            { title: '收藏夹', image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png' },
+            { title: '我', image: 'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png', text: '100', max: '99' }
+          ]}
+          onClick={this.handleClick.bind(this)}
+          current={this.state.current}
+        />
       </View>
     )
   }
